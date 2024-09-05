6341911f9f47a836dfd20efc0801c91c37a7acdd
KirinRyu <dan_ryuichi@outlook.com>
2024-09-05 15:08:29 -0300

    readExcel

--- src/main/java/readExcel/readxlsx.java
+++ src/main/java/readExcel/readxlsx.java
@@ -1,7 +1,48 @@
 package readExcel;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class readxlsx {
     public static void main(String[] args) {
+        String filePath = "C:\\Users\\dan_r\\Project\\APACHE-POI\\teste\\teste.xlsx";
 
+        try (FileInputStream fileInputStream = new FileInputStream(filePath);
+        XSSFWorkbook xssfWorkbook = new XSSFWorkbook(fileInputStream)) {
+
+            Sheet sheet = xssfWorkbook.getSheetAt(0);
+
+            for (Row row : sheet) {
+                for (Cell cell : row) {
+                    switch (cell.getCellType()) {
+                        case STRING:
+                            System.out.println(cell.getStringCellValue() + "\t");
+                            break;
+
+                        case NUMERIC:
+                            System.out.println(cell.getNumericCellValue() + "\t");
+                            break;
+
+                        case BOOLEAN:
+                            System.out.println(cell.getBooleanCellValue() + "\t");
+                            break;
+
+                        default:
+                            System.out.println("Tipo de célula desconhecida");
+                            break;
+                    }
+                }
+                System.out.println();
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
